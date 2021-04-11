f0c067d7110db6130bffa5ec8024bac622c7748e
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-04-10 21:21:00 -0700

    factor out sending of a webhook into a function

--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import "fmt"
 import "net/http"
-import "io"
+import "strings"
 
-type Envelope struct {
-    Message string
+type Webhook struct {
+    url string;
+    httpMethod string;
 }
 
 func main() {
-    resp, err := http.Get("https://webhook.site/57663b0a-12b8-4f6d-a875-c38d30803561")
+    // fmt.Println(sayHi("Marco"))
+    webhook := Webhook{
+        "https://webhook.site/57663b0a-12b8-4f6d-a875-c38d30803561",
+        "POST",
+    }
+
+    sendWebhook(webhook, "testttt")
+}
+
+
+func sendWebhook(webhook Webhook, body string) {
+
+    // https://golang.org/pkg/net/http/#Client.Post
+    _, err := http.Post(webhook.url,  "application/json", strings.NewReader(body))
     if err != nil {
         // handle error
         fmt.Println("Error")
     }
-    defer resp.Body.Close()
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        // handle error
-        fmt.Println("Error")
-    }
-    fmt.Println(body)
+    // http.NewRequest("POST", url, strings.NewReader(form.Encode()))
 }
