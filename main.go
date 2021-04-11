1712d19af6fb2db46f1b6672a10922eb8f781496
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-04-10 20:57:59 -0700

    add basic structure for sending an HTTP request

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import "fmt"
+import "net/http"
+import "io"
 
 type Envelope struct {
     Message string
 }
 
 func main() {
-    fmt.Println(sayHi("Marco"))
+    // fmt.Println(sayHi("Marco"))
+    resp, err := http.Get("https://webhook.site/57663b0a-12b8-4f6d-a875-c38d30803561")
+    if err != nil {
+        // handle error
+        fmt.Println("Error")
+    }
+    defer resp.Body.Close()
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        // handle error
+        fmt.Println("Error")
+    }
+    fmt.Println(body)
 }
 
 func sayHi(person string) string {
