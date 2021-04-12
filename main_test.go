e101cfdc419b16c3cd8f4a1bc53ac53a52402460
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-04-11 19:17:29 -0700

    add tests for registerWebhook function

--- main_test.go
+++ main_test.go
@@ -2,10 +2,56 @@ package main
 
 import "testing"
 
-func TestSayHi(t *testing.T) {
-    expected := "Hi Marco"
-    greeting := sayHi("Marco")
-    if greeting != expected {
-        t.Errorf("Greeting was incorrect, got: '%s', want: '%s'", greeting, expected)
+func TestRegisterWebhook(t *testing.T) {
+
+    /* create a map*/
+    activeWebhooks := make(Registry)
+
+    if len(activeWebhooks) != 0{
+        t.Errorf("Registry does not begin in an empty state")
     }
+
+
+    t.Run("CreatesCategory", func(t *testing.T) {
+        testWebhook := Webhook{
+            "https://example.com",
+            "POST",
+        }
+
+        registerWebhook(activeWebhooks, "test", testWebhook)
+
+        if len(activeWebhooks) != 1 {
+            t.Errorf("Webhook event was not added correctly")
+        }
+
+    })
+    t.Run("Add To Existing Category", func(t *testing.T) {
+        testWebhook2 := Webhook{
+            "https://example.com/2",
+            "POST",
+        }
+
+        registerWebhook(activeWebhooks, "test", testWebhook2)
+
+        if len(activeWebhooks) != 1 {
+            t.Errorf("incorrect modifications to event identifers are present")
+        }
+
+        if len(activeWebhooks["test"]) != 2 {
+            t.Errorf("incorrect number of webhooks present for event id \"test\"")
+        }
+    })
+    t.Run("Empty Webhook", func(t *testing.T) {
+        emptyWebhook := Webhook{}
+
+        registerWebhook(activeWebhooks, "test", emptyWebhook)
+
+        if len(activeWebhooks) != 1 {
+            t.Errorf("incorrect modifications to event identifers are present")
+        }
+
+        if len(activeWebhooks["test"]) != 2 {
+            t.Errorf("incorrect number of webhooks present for event id \"test\"")
+        }
+    })
 }
