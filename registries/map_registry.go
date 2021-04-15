3f9d9ebb3d527e862c52008dd1dc599fb3d7649a
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-04-14 22:08:45 -0700

    First pass at extracting an interface from Registry to allow custom backend storage

---
+++ registries/map_registry.go
@@ -0,0 +1,96 @@
+package registries
+
+import "github.com/MoralCode/go-event-webhooks/models"
+
+type Webhooks []models.Webhook
+
+type MapRegistry map[string]Webhooks
+
+
+/* implement interface methods*/
+func CreateMapRegistry() [MapRegistry] {
+    /* method implementation */
+    return MapRegistry{}
+}
+
+
+func (m_registry MapRegistry) AddToEvent(webhook models.Webhook, eventId string) {
+    values, ok := m_registry[eventId]   
+    /* if ok is true, entry is present otherwise entry is absent*/
+    if (ok) {
+        //TODO: err if exists already?
+        if (webhook != models.Webhook{} && findIndexInList(values, webhook) == -1) {
+                m_registry[eventId] = append(values, webhook)
+        }
+    } else {
+        // before the loop
+        output := []models.Webhook{}
+        output = append(output, webhook)
+        m_registry[eventId] = output
+    }
+}
+
+func (m_registry MapRegistry) RemoveFromEvent(webhook models.Webhook, eventId string) {
+        index := findIndexInList(registry[eventId], webhook)
+
+        if index == -1 {
+            return errors.New("provided webhook is not present in the registry for the given event ID")
+        }
+
+        newlist, err := registry[eventId].removeIndex(index)
+        if err != nil {
+            return err
+        }
+
+        registry[eventId] = newlist
+        return nil
+    }
+
+// inspired by: https://stackoverflow.com/a/37335777/
+func func (m_registry MapRegistry(list Webhooks) removeIndex(index int) (Webhooks, error) {
+    if index < 0 {
+        return list, errors.New("negative indices are not allowed")
+    }
+    
+    if (index >= len(list)) {
+        return list, errors.New("provided index exceeds the bounds of the provided list ")
+    }
+
+    if (index == len(list)-1) {
+        //if the item to remove is the last one, just return all but the last item
+        return list[:len(list)-1], nil
+    }
+
+    //otherwise replace the item to be removed with the last item
+
+    list[index] = list[len(list)-1]
+    // We do not need to put s[i] at the end, as it will be discarded anyway
+    return list[:len(list)-1], nil
+}
+
+func (list Webhooks) findIndexOf(webhook models.Webhook) (int) {
+    for i, n := range list {
+        if webhook == n {
+            return i
+        }
+    }
+    return -1
+}
+
+func (m_registry MapRegistry) Find(webhook models.Webhook) (string, int) {
+    for key, _ := range m_registry {
+        index := m_registry[key].findIndexOf(webhook)
+
+        if index != -1 {
+            return key, index
+        }
+    }
+    return "", -1
+}
+
+func (m_registry MapRegistry) FindInEvent(eventId string, webhook models.Webhook) (int, error) {
+}
+
+func (m_registry MapRegistry) ListEvents() ([]string) {
+
+}
---
+++ registries/registry.go
@@ -0,0 +1,11 @@
+package registries
+
+import "github.com/MoralCode/go-event-webhooks/models"
+
+type Registry interface {
+    AddToEvent(webhook models.Webhook, eventId string) (error)
+    RemoveFromEvent(webhook models.Webhook, eventId string) (error)
+    Find(webhook models.Webhook) (string, int)
+    FindInEvent(eventId string, webhook models.Webhook) (int, error)
+    ListEvents() ([]string)
+}
